596b2d35df1fa385815552881e0f1c2e1d8b0991
Gaetano Barreca <gaetanobarreca@gmail.com>
2019-04-07 12:56:00 +0100

    added separate script file

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('.slick-slider').slick({
+    });
+});
+
+var currentDate = new Date();
+var months = new Array();
+months[0] = "January";
+months[1] = "February";
+months[2] = "March";
+months[3] = "April";
+months[4] = "May";
+months[5] = "June";
+months[6] = "July";
+months[7] = "August";
+months[8] = "September";
+months[9] = "October";
+months[10] = "November";
+months[11] = "December";
+
+var monthString = months[currentDate.getMonth()];
+var stringDate = currentDate.getDay() + ' ' + monthString + ' ' + currentDate.getFullYear();
+$('ins').text(stringDate);
